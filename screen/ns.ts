4ddd8ec09867bb103287cd6485808aef009b8126
Tom Ball <tball@microsoft.com>
2024-02-21 14:45:05 -0800

    reverse order

--- screen/ns.ts
+++ screen/ns.ts
@@ -1,13 +1,13 @@
-//% weight=5 icon="\uf03e" color="#a5b1c2"
-namespace bitmaps {
-
-}
-
 /**
  * Access to game controls
  */
-//% weight=4 color="#D54322" icon="\uf11b"
+//% weight=5 color="#D54322" icon="\uf11b"
 //% blockGap=8
 namespace controller {
+
+}
+
+//% weight=4 icon="\uf03e" color="#a5b1c2"
+namespace bitmaps {
 
 }
