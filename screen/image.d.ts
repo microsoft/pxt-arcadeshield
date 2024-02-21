957a5f36ce74afbdcebf6dcfa5ac505688c912af
Tom Ball <tball@microsoft.com>
2024-02-21 13:04:34 -0800

    update per Richard's suggestion

--- screen/image.d.ts
+++ screen/image.d.ts
@@ -4,7 +4,7 @@ interface Bitmap {
      * Fill a rectangle
      */
     //% helper=imageFillRect blockNamespace="bitmaps" inlineInputMode="inline" group="Drawing"
-    //% block="fill rectangle in %picture=variables_get at x %x y %y width %w height %h %c=colorindexpicker"
+    //% block="fill rectangle in %picture=variables_get at x %x y %y width %w height %h %c=colorNumberPicker"
     //% help=github:microsoft/pxt-arcadeshield/images/image/fill-rect
     fillRect(x: number, y: number, w: number, h: number, c: color): void;
 
@@ -18,7 +18,7 @@ interface Bitmap {
      * Draw a line
      */
     //% helper=imageDrawLine blockNamespace="bitmaps" inlineInputMode="inline" group="Drawing"
-    //% block="draw line in %picture=variables_get from x %x0 y %y0 to x %x1 y %y1 %c=colorindexpicker"
+    //% block="draw line in %picture=variables_get from x %x0 y %y0 to x %x1 y %y1 %c=colorNumberPicker"
     //% help=images/image/draw-line
     drawLine(x0: number, y0: number, x1: number, y1: number, c: color): void;
 
@@ -26,7 +26,7 @@ interface Bitmap {
      * Draw an empty rectangle
      */
     //% helper=imageDrawRect blockNamespace="bitmaps" inlineInputMode="inline" group="Drawing"
-    //% block="draw rectangle in %picture=variables_get at x %x y %y width %w height %h %c=colorindexpicker"
+    //% block="draw rectangle in %picture=variables_get at x %x y %y width %w height %h %c=colorNumberPicker"
     //% help=images/image/draw-rect
     drawRect(x: number, y: number, w: number, h: number, c: color): void;
 
@@ -34,7 +34,7 @@ interface Bitmap {
      * Set pixel color
      */
     //% shim=BitmapMethods::setPixel blockNamespace="bitmaps" group="Drawing"
-    //% block="set %picture=variables_get color at x %x y %y to %c=colorindexpicker"
+    //% block="set %picture=variables_get color at x %x y %y to %c=colorNumberPicker"
     //% help=images/image/set-pixel
     setPixel(x: int32, y: int32, c: int32): void;
 
@@ -50,7 +50,7 @@ interface Bitmap {
      * Fill entire image with a given color
      */
     //% shim=BitmapMethods::fill blockNamespace="bitmaps" group="Drawing"
-    //% block="fill %picture=variables_get with %c=colorindexpicker"
+    //% block="fill %picture=variables_get with %c=colorNumberPicker"
     //% help=images/image/fill
     fill(c: int32): void;
 
@@ -90,7 +90,7 @@ interface Bitmap {
      * Replaces one color in an image with another
      */
     //% shim=BitmapMethods::replace blockNamespace="bitmaps" group="Transformations"
-    //% block="change color in %picture=variables_get from %from=colorindexpicker to %to=colorindexpicker"
+    //% block="change color in %picture=variables_get from %from=colorNumberPicker to %to=colorNumberPicker"
     //% help=images/image/replace
     replace(from: int32, to: int32): void;
 
