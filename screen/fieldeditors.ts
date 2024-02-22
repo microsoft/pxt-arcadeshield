21e38fc975a8b5fd4737fab2c3770b5c748c362c
Tom Ball <tball@microsoft.com>
2024-02-21 16:08:59 -0800

    move palette

--- screen/fieldeditors.ts
+++ screen/fieldeditors.ts
@@ -6,6 +6,7 @@ namespace bitmaps {
     //% blockId=colorindexpicker block="%index" blockHidden=true shim=TD_ID
     //% index.fieldEditor="colornumber"
     //% index.fieldOptions.valueMode="index"
+    //% index.fieldOptions.colours='["#000000","#ffffff","#ff2121","#ff93c4","#ff8135","#fff609","#249ca3","#78dc52","#003fad","#f2f2f2","#f2b233","#e45a33","#a5694f","#7c3f58","#91d2e7","#1e1e1e"]'
     //% index.fieldOptions.decompileLiterals="true"
     export function __colorIndexPicker(index: number) {
         return index;
