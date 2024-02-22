f40396034b395c8006bc4a2177a08c8d9a5161ab
Tom Ball <tball@microsoft.com>
2024-02-21 21:04:09 -0800

    try new path for doc

--- screen/image.d.ts
+++ screen/image.d.ts
@@ -5,7 +5,7 @@ interface Bitmap {
      */
     //% helper=imageFillRect blockNamespace="bitmaps" inlineInputMode="inline" group="Drawing"
     //% block="fill rectangle in %bmap=variables_get at x %x y %y width %w height %h %c=colorindexpicker"
-    //% help=github:microsoft/pxt-arcadeshield/images/image/fill-rect
+    //% help=github:microsoft/pxt-arcadeshield/reference/images/image/fill-rect
     fillRect(x: number, y: number, w: number, h: number, c: color): void;
 
     /**
