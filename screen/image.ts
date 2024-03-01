004a43081bd366546f460d4123b1456f7d112f00
Tom Ball <tball@microsoft.com>
2024-03-01 14:38:05 -0800

    move

--- screen/image.ts
+++ screen/image.ts
@@ -7,7 +7,7 @@ namespace bitmap {
      * @param bitmap bitmap to draw
      */
     //% blockId=displayshowimage block="show bitmap %bitmap=bitmap_picker"
-    //% weight=100 group="Bitmap" blockGap=8
+    //% weight=100 group="Bitmaps" blockGap=8
     //% help=display/show-image
     export function showBitmap(bitmap: Bitmap, duration: number = 400) {
         screen.fill(0);
