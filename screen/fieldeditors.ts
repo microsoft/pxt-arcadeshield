fc4fe88075d504587805956e6fdee08973f090ba
Tom Ball <tball@microsoft.com>
2024-03-01 10:32:34 -0800

    bring back image editing

--- screen/fieldeditors.ts
+++ screen/fieldeditors.ts
@@ -3,6 +3,34 @@
  */
 namespace bitmaps {
 
+    /**
+     * A bitmap
+     * @param bitmap the bitmap
+     */
+    //% blockId=image_picker block="$image" shim=TD_ID
+    //% image.fieldEditor="sprite"
+    //% image.fieldOptions.taggedTemplate="img"
+    //% image.fieldOptions.decompileIndirectFixedInstances="true"
+    //% image.fieldOptions.decompileArgumentAsString="true"
+    //% weight=0 group="Create"
+    //% help=images/image
+    export function _image(image: Image): Image {
+        return image;
+    }
+
+    //% blockId=screen_image_picker block="%img"
+    //% shim=TD_ID
+    //% img.fieldEditor="sprite"
+    //% img.fieldOptions.taggedTemplate="img"
+    //% img.fieldOptions.decompileIndirectFixedInstances="true"
+    //% img.fieldOptions.decompileArgumentAsString="true"
+    //% img.fieldOptions.filter="!tile !dialog !background"
+    //% weight=100 group="Create" duplicateShadowOnDrag
+    //% help=images/sprite-image
+    export function _spriteImage(img: Image) {
+        return img
+    }
+
     //% blockId=colorindexpicker block="%index" blockHidden=true shim=TD_ID
     //% index.fieldEditor="colornumber"
     //% index.fieldOptions.valueMode="index"
--- screen/image.ts
+++ screen/image.ts
@@ -1,6 +1,22 @@
 type color = number
 
 namespace bitmap {
+
+    /**
+     * Show a bitmap on the screen
+     * @param bitmap bitmap to draw
+     */
+    //% blockId=displayshowimage block="show bitmap %bitmap=image_picker"
+    //% weight=100 group="Bitmap" blockGap=8
+    //% help=display/show-image
+    export function showBitmap(bitmap: Bitmap, duration: number = 400) {
+        screen.fill(0);
+        if (bitmap)
+            screen.drawBitmap(bitmap, 0, 0)
+        if (duration > 0)
+            basic.pause(duration);
+    }
+
     export function repeatY(count: number, image: Bitmap) {
         let arr = [image]
         while (--count > 0)
