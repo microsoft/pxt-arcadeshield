413451a8aaefdbf4a2f721c2853f5aaf05c4edaf
Tom Ball <tball@microsoft.com>
2024-06-07 08:16:39 -0700

    change comment

--- targetoverrides.ts
+++ targetoverrides.ts
@@ -5,7 +5,7 @@
 //% groups=["0.","1#","2T","3t","4N","5n","6G","7g","8","9","aAR","bBP","cCp","dDO","eEY","fFW"]
 function bmp(lits: any, ...args: any[]): Bitmap { return null }
 
-// set palette before creating screen, so the JS version has the right BPP
+// set palette before creating screen, which initializes the display
 bitmap.setPalette(hex`000000ffffffff2121ff93c4ff8135fff609249ca378dc52003fad87f2ff8e2ec4a4839f5c406ce5cdc491463d000000`)
 
 //% whenUsed
