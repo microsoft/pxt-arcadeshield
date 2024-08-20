146658fa4a053937c63ba4e154104e754cea53c8
Eric Anderson <eanders@microsoft.com>
2024-08-20 12:09:39 -0700

    trying TD_NOOP

--- screen/sim/screen.ts
+++ screen/sim/screen.ts
@@ -1,36 +1,43 @@
 namespace pxsim.pxtcore {
+    //% TD_NOOP
     export function updateScreen(img: RefImage) {
         const state = pxsim.getScreenState();
         if (state)
             state.showImage(img);
     }
+    //% TD_NOOP
     export function updateStats(s: string) {
         const state = pxsim.getScreenState();
         if (state)
             state.updateStats(s);
     }
+    //% TD_NOOP
     export function setPalette(b: RefBuffer) {
         const state = pxsim.getScreenState();
         if (state)
             state.setPalette(b);
     }
+    //% TD_NOOP
     export function setScreenBrightness(b: number) {
         const state = pxsim.getScreenState();
         if (state)
             state.setScreenBrightness(b);
     }
+    //% TD_NOOP
     export function displayHeight(): number {
         const state = pxsim.getScreenState();
         if (state)
             return state.displayHeight();
         return -1;
     }
+    //% TD_NOOP
     export function displayWidth(): number {
         const state = pxsim.getScreenState();
         if (state)
             return state.displayWidth();
         return -1;
     }
+    //% TD_NOOP
     export function displayPresent(): boolean {
         const state = pxsim.getScreenState();
         if (state)
