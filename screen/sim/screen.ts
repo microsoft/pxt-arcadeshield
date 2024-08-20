8f56c2a4e74576f06d20b01f1666bcae5672589f
Eric Anderson <eanders@microsoft.com>
2024-08-19 21:49:27 -0700

    move

--- screen/sim/screen.ts
+++ screen/sim/screen.ts
@@ -1,38 +1,38 @@
-namespace pxsim {
-    export function updateScreen(img: RefImage) {
-        const state = getScreenState();
+namespace pxsim_pxtcore {
+    export function updateScreen(img: pxsim.RefImage) {
+        const state = pxsim.getScreenState();
         if (state)
             state.showImage(img);
     }
     export function updateStats(s: string) {
-        const state = getScreenState();
+        const state = pxsim.getScreenState();
         if (state)
             state.updateStats(s);
     }
-    export function setPalette(b: RefBuffer) {
-        const state = getScreenState();
+    export function setPalette(b: pxsim.RefBuffer) {
+        const state = pxsim.getScreenState();
         if (state)
             state.setPalette(b);
     }
     export function setScreenBrightness(b: number) {
-        const state = getScreenState();
+        const state = pxsim.getScreenState();
         if (state)
             state.setScreenBrightness(b);
     }
     export function displayHeight(): number {
-        const state = getScreenState();
+        const state = pxsim.getScreenState();
         if (state)
             return state.displayHeight();
         return 0;
     }
     export function displayWidth(): number {
-        const state = getScreenState();
+        const state = pxsim.getScreenState();
         if (state)
             return state.displayWidth();
         return 0;
     }
     export function displayPresent(): boolean {
-        const state = getScreenState();
+        const state = pxsim.getScreenState();
         if (state)
             return state.displayPresent();
         return false;
