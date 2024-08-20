0b252c05ae03a6990a8767563ee50e2120d95912
Eric Anderson <eanders@microsoft.com>
2024-08-20 12:13:03 -0700

    ref globals

--- screen/sim/screen.ts
+++ screen/sim/screen.ts
@@ -1,43 +1,47 @@
+//% fixedInstance
+const refs = {
+    updateScreen: pxsim.pxtcore.updateScreen,
+    updateStats: pxsim.pxtcore.updateStats,
+    setPalette: pxsim.pxtcore.setPalette,
+    setScreenBrightness: pxsim.pxtcore.setScreenBrightness,
+    displayHeight: pxsim.pxtcore.displayHeight,
+    displayWidth: pxsim.pxtcore.displayWidth,
+    displayPresent: pxsim.pxtcore.displayPresent
+};
+
 namespace pxsim.pxtcore {
-    //% TD_NOOP
     export function updateScreen(img: RefImage) {
         const state = pxsim.getScreenState();
         if (state)
             state.showImage(img);
     }
-    //% TD_NOOP
     export function updateStats(s: string) {
         const state = pxsim.getScreenState();
         if (state)
             state.updateStats(s);
     }
-    //% TD_NOOP
     export function setPalette(b: RefBuffer) {
         const state = pxsim.getScreenState();
         if (state)
             state.setPalette(b);
     }
-    //% TD_NOOP
     export function setScreenBrightness(b: number) {
         const state = pxsim.getScreenState();
         if (state)
             state.setScreenBrightness(b);
     }
-    //% TD_NOOP
     export function displayHeight(): number {
         const state = pxsim.getScreenState();
         if (state)
             return state.displayHeight();
         return -1;
     }
-    //% TD_NOOP
     export function displayWidth(): number {
         const state = pxsim.getScreenState();
         if (state)
             return state.displayWidth();
         return -1;
     }
-    //% TD_NOOP
     export function displayPresent(): boolean {
         const state = pxsim.getScreenState();
         if (state)
