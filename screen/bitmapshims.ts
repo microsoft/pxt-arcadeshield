35acff76e24b2cc7d4ed081d406739b326626062
Tom Ball <tball@microsoft.com>
2024-08-20 15:49:51 -0700

    more work on empty shims

--- screen/bitmapshims.ts
+++ screen/bitmapshims.ts
@@ -1,18 +1,22 @@
 //% fixedInstances decompileIndirectFixedInstances
-interface Bitmap {
+namespace Bitmap {
     /**
      * Fill a rectangle
      */
     //% helper=imageFillRect blockNamespace="bitmaps" inlineInputMode="inline" group="Drawing"
     //% block="fill rectangle in %bmap=variables_get at x %x y %y width %w height %h %c=colorindexpicker"
     //% help=github:pxt-arcadeshield/reference/bitmaps/bitmap/fill-rect
-    fillRect(x: number, y: number, w: number, h: number, c: color): void;
+    export function fillRect(x: number, y: number, w: number, h: number, c: color): void {
+        basic.pause(0)
+    }
 
     /**
      * Replace colors in a rectangle
      */
     //% helper=imageMapRect
-    mapRect(x: number, y: number, w: number, h: number, colorMap: Buffer): void;
+    export function mapRect(x: number, y: number, w: number, h: number, colorMap: Buffer): void {
+        basic.pause(0)    
+    }
 
     /**
      * Draw a line
@@ -20,7 +24,10 @@ interface Bitmap {
     //% helper=imageDrawLine blockNamespace="bitmaps" inlineInputMode="inline" group="Drawing"
     //% block="draw line in %bmap=variables_get from x %x0 y %y0 to x %x1 y %y1 %c=colorindexpicker"
     //% help=images/image/draw-line
-    drawLine(x0: number, y0: number, x1: number, y1: number, c: color): void;
+    export function drawLine(x0: number, y0: number, x1: number, y1: number, c: color): void 
+    {
+        basic.pause(0)
+    }
 
     /**
      * Draw an empty rectangle
@@ -28,7 +35,9 @@ interface Bitmap {
     //% helper=imageDrawRect blockNamespace="bitmaps" inlineInputMode="inline" group="Drawing"
     //% block="draw rectangle in %bmap=variables_get at x %x y %y width %w height %h %c=colorindexpicker"
     //% help=images/image/draw-rect
-    drawRect(x: number, y: number, w: number, h: number, c: color): void;
+    export function drawRect(x: number, y: number, w: number, h: number, c: color): void {
+        basic.pause(0)
+    }
 
     /**
      * Set pixel color
@@ -36,7 +45,9 @@ interface Bitmap {
     //% shim=BitmapMethods::setPixel blockNamespace="bitmaps" group="Drawing"
     //% block="set %bmap=variables_get color at x %x y %y to %c=colorindexpicker"
     //% help=images/image/set-pixel
-    setPixel(x: int32, y: int32, c: int32): void;
+    export function setPixel(x: int32, y: int32, c: int32): void {
+        basic.pause(0)
+    }
 
     /**
      * Get a pixel color
@@ -44,7 +55,9 @@ interface Bitmap {
     //% shim=BitmapMethods::getPixel blockNamespace="bitmaps" group="Drawing"
     //% block="%bmap=variables_get color at x %x y %y"
     //% help=images/image/get-pixel
-    getPixel(x: int32, y: int32): int32;
+    export function getPixel(x: int32, y: int32): int32 {
+        return 0
+    }
 
     /**
      * Fill entire image with a given color
@@ -52,7 +65,9 @@ interface Bitmap {
     //% shim=BitmapMethods::fill blockNamespace="bitmaps" group="Drawing"
     //% block="fill %bmap=variables_get with %c=colorindexpicker"
     //% help=images/image/fill
-    fill(c: int32): void;
+    export function fill(c: int32): void {
+        basic.pause(0)
+    }
 
     /**
      * Return a copy of the current image
@@ -61,7 +76,9 @@ interface Bitmap {
     //% weight=60
     //% block="clone %bmap=variables_get"
     //% help=images/image/clone
-    clone(): Bitmap;
+    export function clone(): Bitmap {
+        return null;
+    }
 
     /**
      * Flips (mirrors) pixels horizontally in the current image
@@ -69,7 +86,9 @@ interface Bitmap {
     //% shim=BitmapMethods::flipX blockNamespace="bitmaps" group="Transformations"
     //% block="flip %bmap=variables_get horizontally"
     //% help=images/image/flip-x
-    flipX(): void;
+    export function flipX(): void {
+        basic.pause(0)
+    }
 
     /**
      * Flips (mirrors) pixels vertically in the current image
@@ -77,14 +96,18 @@ interface Bitmap {
     //% shim=BitmapMethods::flipY blockNamespace="bitmaps" group="Transformations"
     //% block="flip %bmap=variables_get vertically"
     //% help=images/image/flip-y
-    flipY(): void;
+    export function flipY(): void {
+        basic.pause(0)
+    }
 
     /**
      * Every pixel in image is moved by (dx,dy)
      */
     //% shim=BitmapMethods::scroll blockNamespace="bitmaps" group="Transformations"
     //% help=images/image/scroll
-    scroll(dx: int32, dy: int32): void;
+    export function scroll(dx: int32, dy: int32): void {
+        basic.pause(0)
+    }
 
     /**
      * Replaces one color in an image with another
@@ -92,7 +115,9 @@ interface Bitmap {
     //% shim=BitmapMethods::replace blockNamespace="bitmaps" group="Transformations"
     //% block="change color in %bmap=variables_get from %from=colorindexpicker to %to=colorindexpicker"
     //% help=images/image/replace
-    replace(from: int32, to: int32): void;
+    export function replace(from: int32, to: int32): void {
+        basic.pause(0)
+    }
 
     /**
      * Returns true if the provided image is the same as this image,
@@ -105,19 +130,27 @@ interface Bitmap {
     //% other.shadow=variables_get
     //% this.defl="bmap"
     //% help=images/image/equals
-    equals(other: Bitmap): boolean;
+    export function equals(other: Bitmap): boolean {
+        return false
+    }
 
     //% shim=BitmapMethods::isStatic
-    isStatic(): boolean;
+    export function isStatic(): boolean {
+        return false
+    }
 
     //% shim=BitmapMethods::revision
-    revision(): number;
+    export function revision(): number {
+        return 0
+    }
 }
 
-declare namespace bitmap {
+namespace bitmap {
     //% blockNamespace="bitmaps"
     //% block="create bitmap width %width height %height" group="Create"
     //% weight=50
     //% help=images/create
-    function create(width: number, height: number): Bitmap;
+    function create(width: number, height: number): Bitmap {
+        return null;
+    }
 }
