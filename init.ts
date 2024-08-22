4d10a54d1d07f5ca7d7a9aaebb24bf8bfd8bc3c7
Tom Ball <tball@microsoft.com>
2024-08-22 15:28:54 -0700

    more fun

--- init.ts
+++ init.ts
@@ -12,15 +12,13 @@ screenhelpers.setPalette(hex`000000ffffffff2121ff93c4ff8135fff609249ca378dc52003
 const screen: ScreenBitmap = __screen_internal.createScreen();
 
 namespace __screen_internal {
-    //% shim=pxt::updateScreen
-    function updateScreen(img: Bitmap): void { }
 
     export function createScreen() {
         const img = bitmap.create(
             screenhelpers.displayWidth(), // control.getConfigValue(DAL.CFG_DISPLAY_WIDTH, 160)
             screenhelpers.displayHeight() // control.getConfigValue(DAL.CFG_DISPLAY_HEIGHT, 128)
         )
-        control.__screen.setupUpdate(() => updateScreen(img))
+        control.__screen.setupUpdate(() => screenhelpers.updateScreen(img))
         return img as ScreenBitmap;
     }
 }
--- sim/state.ts
+++ sim/state.ts
@@ -51,9 +51,8 @@ namespace pxsim {
                 broadcast: false,
                 channel: "arcadeshield",
                 data: buf
-            } as SimulatorControlMessage;
-            console.log(fullmsg)
-            Runtime.postMessage(fullmsg)
+            }
+            //control.simmessages.send("jacdac", JSON.stringify(fullmsg) payload, false)
         }
 
         setScreenBrightness(b: number) {
@@ -71,7 +70,7 @@ namespace pxsim {
             const msg: _protocol.SetPaletteMessage = {
                 type: "set-palette",
                 runId: this.runId,
-                data: buf.data.toString()
+                data: null //buf.data.toString()
             }
             this.sendMessage(JSON.stringify(msg))
         }
@@ -95,5 +94,46 @@ namespace pxsim {
 
     export function getScreenState(): ScreenState {
         return _screenState;
+    }
+}
+
+namespace pxsim {
+    export function updateScreen(img: Bitmap) {
+        const state = getScreenState();
+        if (state)
+            state.showImage(img);
+    }
+    export function updateStats(s: string) {
+        const state = getScreenState();
+        if (state)
+            state.updateStats(s);
+    }
+    export function setPalette(b: Buffer) {
+        const state = getScreenState();
+        if (state)
+            state.setPalette(b);
+    }
+    export function setScreenBrightness(b: number) {
+        const state = getScreenState();
+        if (state)
+            state.setScreenBrightness(b);
+    }
+    export function displayHeight(): number {
+        const state = getScreenState();
+        if (state)
+            return state.displayHeight();
+        return -1;
+    }
+    export function displayWidth(): number {
+        const state = getScreenState();
+        if (state)
+            return state.displayWidth();
+        return -1;
+    }
+    export function displayPresent(): boolean {
+        const state = getScreenState();
+        if (state)
+            return state.displayPresent();
+        return false;
     }
 }
