605a0537eef76fc0be15d1e3c339019add685fc1
Tom Ball <tball@microsoft.com>
2024-08-24 18:47:59 -0700

    typo

--- bitmap.d.ts
+++ bitmap.d.ts
@@ -6,7 +6,7 @@ interface Bitmap {
     //% helper=imageFillRect blockNamespace="bitmaps" inlineInputMode="inline" group="Drawing"
     //% block="fill rectangle in %bmap=variables_get at x %x y %y width %w height %h %c=colorindexpicker"
     //% help=github:microsoft/pxt-arcadeshield/reference/bitmaps/bitmap/fill-rect
-    //% bmap.shaow="theScreen"
+    //% bmap.shadow="theScreen"
     fillRect(x: number, y: number, w: number, h: number, c: color): void;
 
     /**
