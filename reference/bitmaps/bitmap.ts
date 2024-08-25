674d52ae87a667ccbf2e354bd4ab4c451b3cc0a0
Tom Ball <tball@microsoft.com>
2024-08-24 17:15:31 -0700

    try this instead

---
+++ reference/bitmaps/bitmap.ts
@@ -0,0 +1,12 @@
+
+namespace bitmap {
+    /**
+    * Get the screen bitmap
+    */
+    //% blockNamespace="bitmaps" group="Create"
+    //% blockId=imagescreen block="screen"
+    //% help=bitmaps/screen-image
+    export function screenImage(): Image {
+        return screen;
+    }
+}
