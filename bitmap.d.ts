85bd4aa317d1a73347522f5f85f1c712926fbecb
Tom Ball <tball@microsoft.com>
2024-08-25 15:39:31 -0700

    redo path

--- bitmap.d.ts
+++ bitmap.d.ts
@@ -5,7 +5,7 @@ interface Bitmap {
      */
     //% helper=imageFillRect blockNamespace="bitmaps" inlineInputMode="inline" group="Drawing"
     //% block="fill rectangle in %bmap=variables_get at x %x y %y width %w height %h %c=colorindexpicker"
-    //% help=github:microsoft/pxt-arcadeshield/reference/bitmaps/bitmap/fill-rect
+    //% help=github:microsoft/pxt-arcadeshield/docs/reference/bitmaps/bitmap/fill-rect
     //% bmap.shadow="theScreen"
     fillRect(x: number, y: number, w: number, h: number, c: color): void;
 
