37a8b5598735bb28d74e9b779c9647b7c7ac5f4d
Eric Anderson <eanders@microsoft.com>
2024-08-26 18:16:30 -0700

    try something

--- init.ts
+++ init.ts
@@ -8,7 +8,6 @@ function bmp(lits: any, ...args: any[]): Bitmap { return null; }
 // set palette before creating screen, which initializes the display
 screenhelpers.setPalette(hex`000000ffffffff2121ff93c4ff8135fff609249ca378dc52003fad87f2ff8e2ec4a4839f5c406ce5cdc491463d000000`)
 
-//% whenUsed
 const theScreen: Bitmap = __screen_internal.createScreen();
 
 namespace __screen_internal {
