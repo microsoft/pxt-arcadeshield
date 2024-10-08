12386292b4a4f98c8139d2aa40d46f218da2fad7
Tom Ball (MSR) <tball@microsoft.com>
2024-10-08 12:10:25 -0700

    add fill circle

--- bitmap.d.ts
+++ bitmap.d.ts
@@ -4,7 +4,7 @@ interface Bitmap {
      * Fill entire image with a given color
      */
     //% shim=BitmapMethods::fill blockNamespace="bitmaps" group="Drawing"
-    //% block="fill $this=variables_get with $c=colorindexpicker"
+    //% block="fill $this with $c=colorindexpicker"
     //% help=github:pxt-arcadeshield/docs/reference/bitmaps/bitmap/fill
     //% this.shadow="theScreen"
     //% weight=100
@@ -14,7 +14,7 @@ interface Bitmap {
      * Set pixel color
      */
     //% shim=BitmapMethods::setPixel blockNamespace="bitmaps" group="Drawing"
-    //% block="set $this=variables_get color at x $x y $y to $c=colorindexpicker"
+    //% block="set $this color at x $x y $y to $c=colorindexpicker"
     //% help=github:pxt-arcadeshield/docs/reference/bitmaps/bitmap/set-pixel
     //% this.shadow="theScreen"
     //% weight=96
@@ -24,7 +24,7 @@ interface Bitmap {
      * Get a pixel color
      */
     //% shim=BitmapMethods::getPixel blockNamespace="bitmaps" group="Drawing"
-    //% block="$this=variables_get color at x $x y $y"
+    //% block="$this color at x $x y $y"
     //% help=github:pxt-arcadeshield/docs/reference/bitmaps/bitmap/get-pixel
     //% this.shadow="theScreen"
     //% weight=92
@@ -34,7 +34,7 @@ interface Bitmap {
      * Draw a line
      */
     //% helper=imageDrawLine blockNamespace="bitmaps" inlineInputMode="inline" group="Drawing"
-    //% block="draw line in $this=variables_get from x $x0 y $y0 to x $x1 y $y1 $c=colorindexpicker"
+    //% block="draw line in $this from x $x0 y $y0 to x $x1 y $y1 $c=colorindexpicker"
     //% help=github:pxt-arcadeshield/docs/reference/bitmaps/bitmap/draw-line
     //% this.shadow="theScreen"
     //% weight=88
@@ -44,7 +44,7 @@ interface Bitmap {
      * Draw an empty rectangle
      */
     //% helper=imageDrawRect blockNamespace="bitmaps" inlineInputMode="inline" group="Drawing"
-    //% block="draw rectangle in $this=variables_get at x $x y $y width $w height $h $c=colorindexpicker"
+    //% block="draw rectangle in $this at x $x y $y width $w height $h $c=colorindexpicker"
     //% help=github:pxt-arcadeshield/docs/reference/bitmaps/bitmap/draw-rect
     //% this.shadow="theScreen"
     //% weight=84
@@ -54,7 +54,7 @@ interface Bitmap {
      * Fill a rectangle
      */
     //% helper=imageFillRect blockNamespace="bitmaps" inlineInputMode="inline" group="Drawing"
-    //% block="fill rectangle in $this=variables_get at x $x y $y width $w height $h $c=colorindexpicker"
+    //% block="fill rectangle in $this at x $x y $y width $w height $h $c=colorindexpicker"
     //% help=github:pxt-arcadeshield/docs/reference/bitmaps/bitmap/fill-rect
     //% this.shadow="theScreen"
     //% weight=80
@@ -80,11 +80,31 @@ interface Bitmap {
     //% group="Drawing"
     drawCircle(cx: number, cy: number, r: number, c: color): void;
 
+        /**
+     * Fills a circle
+     *
+     * @param cx The center x coordinate of the circle
+     * @param cy The center y coordinate of the circle
+     * @param r The radius of the circle
+     * @param c The color to draw the circle
+     */
+    //% helper=imageFillCircle
+    //% blockId=image_fill_circle
+    //% block="fill circle in $this at cx $cx cy $cy radius $r $c"
+    //% this.shadow=theScreen
+    //% this.defl=picture
+    //% c.shadow=colorindexpicker
+    //% inlineInputMode="inline"
+    //% weight=77
+    //% blockNamespace="bitmaps"
+    //% group="Drawing"
+    fillCircle(cx: number, cy: number, r: number, c: color): void;
+
     /**
      * Draw given bitmap on the current bitmap
      */
     //% shim=BitmapMethods::drawTransparentBitmap blockNamespace="bitmaps" group="Drawing"
-    //% block="draw bitmap $from=variables_get in $this=variables_get at x $x y $y"
+    //% block="draw bitmap $from=variables_get in $this at x $x y $y"
     //% help=github:pxt-arcadeshield/docs/reference/bitmaps/bitmap/draw-bitmap
     //% this.shadow="theScreen"
     //% weight=76
@@ -94,7 +114,7 @@ interface Bitmap {
      * Flips (mirrors) pixels horizontally in the current image
      */
     //% shim=BitmapMethods::flipX blockNamespace="bitmaps" group="Transformations"
-    //% block="flip $this=variables_get horizontally"
+    //% block="flip $this horizontally"
     //% help=github:pxt-arcadeshield/docs/reference/bitmaps/bitmap/flip-x
     //% this.shadow="theScreen"
     //% weight=72
@@ -104,7 +124,7 @@ interface Bitmap {
      * Flips (mirrors) pixels vertically in the current image
      */
     //% shim=BitmapMethods::flipY blockNamespace="bitmaps" group="Transformations"
-    //% block="flip $this=variables_get vertically"
+    //% block="flip $this vertically"
     //% help=github:pxt-arcadeshield/docs/reference/bitmaps/bitmap/flip-y
     //% this.shadow="theScreen"
     //% weight=68
@@ -116,7 +136,7 @@ interface Bitmap {
     //% shim=BitmapMethods::scroll blockNamespace="bitmaps" group="Transformations"
     //% help=github:pxt-arcadeshield/docs/reference/bitmaps/bitmap/scroll
     //% this.shadow="theScreen"
-    //% block="scroll $this=variables_get by x $dx y $dy"
+    //% block="scroll $this by x $dx y $dy"
     //% weight=64
     scroll(dx: int32, dy: int32): void;
 
@@ -124,7 +144,7 @@ interface Bitmap {
      * Replaces one color in an image with another
      */
     //% shim=BitmapMethods::replace blockNamespace="bitmaps" group="Transformations"
-    //% block="change color in $this=variables_get from $from=colorindexpicker to $to=colorindexpicker"
+    //% block="change color in $this from $from=colorindexpicker to $to=colorindexpicker"
     //% help=bitmaps/bitmap/replace
     //% help=github:pxt-arcadeshield/docs/reference/bitmaps/bitmap/replace
     //% this.shadow="theScreen"
@@ -161,7 +181,7 @@ interface Bitmap {
      */
     //% shim=BitmapMethods::clone blockNamespace="bitmaps" group="Create"
     //% weight=50
-    //% block="clone $this=variables_get"
+    //% block="clone $this"
     //% help=github:pxt-arcadeshield/docs/reference/bitmaps/bitmap/clone
     //% this.shadow="theScreen"
     clone(): Bitmap;
