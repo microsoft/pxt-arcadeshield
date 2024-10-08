92f5588fd7963d851347cde717a89d753549cdc2
Tom Ball (MSR) <tball@microsoft.com>
2024-10-08 13:45:17 -0700

    add width/height

--- bitmap.ts
+++ bitmap.ts
@@ -7,3 +7,32 @@
 function screen(): Bitmap {
         return theScreen;
 }
+
+
+namespace bitmap {
+        export enum Dimension {
+                //% block="width"
+                Width,
+                //% block="height"
+                Height
+            }
+
+        
+        /**
+         * Returns the width or height of a picture.
+         *
+         * @param picture The picture to get the width or height of
+         * @param dimension The dimension to get
+         * @returns
+         */
+        //% blockId=image_get_dimension
+        //% group="Create"
+        //% blockNamespace="bitmaps"
+        //% block="$picture $dimension"
+        //% picture.shadow=variables_get
+        //% picture.defl=picture
+        export function getDimension(picture: Bitmap, dimension: Dimension) {
+                if (dimension === Dimension.Width) return picture.width;
+                else return picture.height;
+        }
+}
