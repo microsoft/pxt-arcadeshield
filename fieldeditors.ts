6ac1d83abb1a2e9294aef032c4f93a83502534af
Tom Ball (MSR) <tball@microsoft.com>
2024-10-09 11:17:34 -0700

    try rename here

--- fieldeditors.ts
+++ fieldeditors.ts
@@ -1,7 +1,7 @@
 /**
  * Bitmap manipulation blocks
  */
-namespace bitmaps {
+namespace drawing {
 
     /**
      * A bitmap
@@ -28,7 +28,7 @@ namespace bitmaps {
     //% index.fieldOptions.valueMode="index"
     //% index.fieldOptions.colours='["#000000","#ffffff","#ff2121","#ff93c4","#ff8135","#fff609","#249ca3","#78dc52","#003fad","#87f2ff","#8e2ec4","#a4839f","#5c406c","#e5cdc4","#91463d","#000000"]'
     //% index.fieldOptions.decompileLiterals="true"
-    export function _colorIndexPicker(index: number) {
+    export function __colorIndexPicker(index: number) {
         return index;
     }
 }
