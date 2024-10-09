e693d672b50f256557a60d723f8c85c390be2a64
Tom Ball (MSR) <tball@microsoft.com>
2024-10-09 11:21:48 -0700

    more hacking

--- fieldeditors.ts
+++ fieldeditors.ts
@@ -1,14 +1,15 @@
 /**
  * Bitmap manipulation blocks
  */
-namespace drawing {
+namespace bitmaps {
 
     /**
      * A bitmap
      * @param bitmap the bitmap
      */
     //% blockId=bitmap_picker 
-    //% block = "$bitmap" shim = TD_ID
+    //% block = "$bitmap" 
+    //% shim = TD_ID
     //% bitmap.fieldEditor="sprite"
     //% bitmap.fieldOptions.taggedTemplate="bmp"
     //% bitmap.fieldOptions.decompileIndirectFixedInstances="true"
@@ -23,16 +24,19 @@ namespace drawing {
 
 
     //% blockId=colorindexpicker 
-    //% block = "$index" blockHidden = true shim = TD_ID
+    //% block = "$index" 
+    //% blockHidden = true 
+    //% shim = TD_ID
     //% index.fieldEditor="colornumber"
     //% index.fieldOptions.valueMode="index"
     //% index.fieldOptions.colours='["#000000","#ffffff","#ff2121","#ff93c4","#ff8135","#fff609","#249ca3","#78dc52","#003fad","#87f2ff","#8e2ec4","#a4839f","#5c406c","#e5cdc4","#91463d","#000000"]'
     //% index.fieldOptions.decompileLiterals="true"
+    //% blockNamespace="drawing"
     export function __colorIndexPicker(index: number) {
         return index;
     }
 }
-    //% fixedInstance blockIdentity=drawing._bitmap
+    //% fixedInstance blockIdentity=bitmaps._bitmap
     const defaultBitmap: Bitmap = bmp`
     . . . . . . . . . . . . . . . .
     . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
