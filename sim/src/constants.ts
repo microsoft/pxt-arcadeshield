632e76970c78cc530fa66a0799d03f3931935dac
Tom Ball <tball@microsoft.com>
2024-11-25 10:58:14 -0800

    remove code

--- sim/src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const DEFAULT_SKIN = "shield-b"
-export const DEFAULT_WIDTH = 160
-export const DEFAULT_HEIGHT = 120
--- sim/src/util.ts
+++
@@ -1,8 +0,0 @@
-export function classList(...classes: (string | undefined)[]) {
-    return classes
-        .filter((c) => typeof c === "string")
-        .reduce((prev, c) => prev.concat(c.split(" ")), [] as string[])
-        .map((c) => c.trim())
-        .filter((c) => !!c)
-        .join(" ")
-}
